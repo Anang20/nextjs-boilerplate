dab5c0b26ab4c46166d90a32174065aad9fe7463
anangsyah <anang.haqim@ist.id>
2023-09-01 23:35:07 +0700

    chore: add toggle button theme mode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
+import { ModeToggle } from '@/components/atoms/buttons/ModeToggle'
 import { DatePicker } from '@/components/atoms/input/DatePicker'
 import Image from 'next/image'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <DatePicker/>
+      <DatePicker />
+      <ModeToggle/>
     </main>
   )
 }
